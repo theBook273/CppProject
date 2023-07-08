4bfa22da70f607f7741bca5803e17984385ef094
theBook273 <votansach273@gmail.com>
2023-07-08 19:27:19 +0700

    oddEven added

--- oddEven/solve.cpp
+++ oddEven/solve.cpp
@@ -20,4 +20,18 @@ int main()
 
     a[0] = x;
     a[1] = y;
+
+    for (int i = 2; i < n; i++)
+    {
+        if ((i + 1) % 2 == 0)
+        {
+            a[i] = (a[i - 1] + a[i - 2]) % k;
+        }
+        else
+        {
+            a[i] = abs(a[i - 1] - a[i - 2]) % k;
+        }
+    }
+
+    cout << a[n - 1] << "\n";
 }
