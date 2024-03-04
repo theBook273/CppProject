3f1120ef28546869b4dcfcc4b1f81995513bb575
theBook273 <votansach273@gmail.com>
2024-03-04 18:03:16 +0700

    update

--- Test/baihoc.cpp
+++ Test/baihoc.cpp
@@ -1,29 +1,14 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
-using ll = long long;
-string s;
 
-bool so(string s) {
-    while (s.size() > 1) {
-        ll tong = 0;
+int main() {
+    int a, b, sum = 0;
 
-        for (ll i = 0; i < s.size(); i++) {
-            double temp = pow(s[i] - '0', 2);
-            tong += temp;
-        }
+    cin >> a >> b;
+    sum = a + b;
 
-        s = to_string(tong);
-    }
-    // cout << s << endl;
-    return s == "1";
-}
+    cout << "sum of " << a << " and " << b << " is " << sum << endl;
 
-signed main() {
-    cin >> s;
-    so(s);
-    // s = to_string(stoi(s) + 1);
-    // while (!so(s)) {
-    //     s = to_string(stoi(s) + 1);
-    // }
-    // cout << s << endl;
+    return 0;
 }
