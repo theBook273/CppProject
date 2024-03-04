a1d2dd368ad96a36a3bc9b94f0fec5a43f579c6b
theBook273 <114481981+theBook273@users.noreply.github.com>
2024-03-04 14:56:22 +0700

    Update solve3.cpp

--- cachNhiet/solve3.cpp
+++ cachNhiet/solve3.cpp
@@ -1,24 +1,34 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 using ll = long long;
-string s;
+long long s;
+
+long long changed(int n) {
+    long long res = 0;
+    while (n / 10 > 0) {
+        double temp = (pow(n % 10, 2));
+        res += temp;
+        n /= 10;
+    }
+    if (n != 0) {
+        double temp = (pow(n, 2));
+        res += temp;
+    }
+    return res;
+}
 
 bool so(ll n) {
-    double temp = n;
-    while (n / 10 > 0) {
-        n = 0;
-        while (temp >= 1) {
-            temp +=
-        }
-    }
+    // double temp = n;
+    // while (n / 10 > 0) {
+    //     n = 0;
+    //     while (temp >= 1) {
+    //         temp +=
+    //     }
+    // }
 }
 
 signed main() {
     cin >> s;
-
-    s = to_string(stoi(s) + 1);
-    while (!so(s)) {
-        s = to_string(stoi(s) + 1);
-    }
-    cout << s << endl;
+    cout << changed(s);
 }
