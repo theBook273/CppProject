5e9cbdd951c726b2ca79cf96ccf2a0228ae7a454
theBook273 <votansach273@gmail.com>
2025-03-15 13:18:12 +0700

    update

--- demcap/solve.cpp
+++ demcap/solve.cpp
@@ -5,17 +5,29 @@ using namespace std;
 
 const int maxn = 5 * 1e5 + 8;
 
-int a[maxn];
+int a[maxn], mark[maxn];
+int n;
 
-signed main() {
-    cin.tie(NULL)->sync_with_stdio(false);
-    int n;
+main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
+    int res = 0;
     for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
+        int maxnow = 0;
+        for (int j = i + 1; j < n; j++) {
+            if (i + 1 == j) {
+                res++;
+            } else {
+                maxnow = max(maxnow, a[j - 1]);
+                if (a[i] < maxnow || a[j] < maxnow) {
+                    break;
+                } else
+                    res++;
+            }
+        }
     }
+    cout << res << endl;
 }
