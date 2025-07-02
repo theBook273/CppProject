e0a76171efaab15527071b46d31782624b36006c
theBook273 <votansach273@gmail.com>
2025-07-02 21:35:56 +0700

    update

--- Test/test.cpp
+++ Test/test.cpp
@@ -2,18 +2,4 @@
 
 using namespace std;
 
-signed main() {
-  int l = 1, r = 100;
-
-  while (l <= r) {
-    int m = (l + r) / 2;
-    cout << m << "\n";
-    bool beHon;
-    cin >> beHon;
-    if (beHon) {
-      r = m - 1;
-    } else {
-      l = m + 1;
-    }
-  }
-}
+int main() { cout << "hello world"; }
--- sumThan10/solve.cpp
+++ sumThan10/solve.cpp
@@ -2,24 +2,28 @@
 
 using namespace std;
 
-int main() {
-  int n;
-  cin >> n;
+int main()
+{
+    int n;
+    cin >> n;
 
-  while (n--) {
-    int a[3];
+    while (n--)
+    {
+        int a[3];
 
-    for (int i = 0; i < 3; i++) {
-      cin >> a[i];
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> a[i];
+        }
+
+        sort(a, a + 3);
+        reverse(a, a + 3);
+
+        if (a[0] + a[1] >= 10)
+        {
+            cout << "YES" << endl;
+            continue;
+        }
+        cout << "NO" << endl;
     }
-
-    sort(a, a + 3);
-    reverse(a, a + 3);
-
-    if (a[0] + a[1] >= 10) {
-      cout << "YES" << endl;
-      continue;
-    }
-    cout << "NO" << endl;
-  }
 }
