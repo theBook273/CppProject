fde004a4e27b00ddbcba534ab87889a5dad588e8
theBook273 <votansach273@gmail.com>
2025-07-02 22:01:58 +0700

    update

---
+++ codeForces1034/binBattle.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+  string ans[] = {"Bob\n", "Alice\n"};
+
+  int t;
+  cin >> t;
+  while (t--) {
+    bool flag = 0;
+    int n, k;
+    cin >> n >> k;
+
+    vector<char> temp(n);
+    vector<int> a;
+
+    for (char &i : temp) {
+      cin >> i;
+      a.push_back(i - '0');
+    }
+
+    int tong = 0;
+
+    for (auto i : a) {
+      tong += i;
+    }
+
+    if (tong <= k) {
+      cout << ans[1];
+      continue;
+    }
+
+    if (n % 2 != 0) {
+      cout << ans[(k > ((n - 1) / 2))];
+    } else {
+      cout << ans[(k >= ((n + 2) / 2))];
+    }
+  }
+}
