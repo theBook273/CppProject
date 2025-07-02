86c41c2c2af6aa48f5438f776039927ca94d4c7d
theBook273 <votansach273@gmail.com>
2025-07-03 00:55:30 +0700

    update

--- linkedlist/allAboutSinglyLinkedList.cpp
+++ linkedlist/allAboutSinglyLinkedList.cpp
@@ -16,14 +16,11 @@ Node *createNode(long long val) {
 
 void insertBack(Node *&head, long long val) {
   Node *newOne = createNode(val);
-
   if (head == nullptr) {
     head = newOne;
     return;
   }
-
   Node *temp = head;
-
   while (temp->next != nullptr) {
     temp = temp->next;
   }
@@ -35,18 +32,14 @@ Node *insertPos(Node *head, long long n, long long pos, long long val) {
     insertBack(head, val);
     return head;
   }
-
   if (pos == 0) {
     Node *newHead = createNode(val);
     newHead->next = head;
     return newHead;
   }
-
   Node *p1 = head, *p2 = head;
   long long cnt = 1;
-
   p2 = p2->next;
-
   while (p2 != nullptr && cnt <= pos) {
     if (cnt == pos) {
       Node *temp = createNode(val);
@@ -58,23 +51,19 @@ Node *insertPos(Node *head, long long n, long long pos, long long val) {
     p2 = p2->next;
     p1 = p1->next;
   }
-
   return head;
 }
 
 Node *removeNode(Node *head, long long pos) {
   Node *dummy = createNode(0);
   dummy->next = head;
-
   Node *p1, *p2;
   p1 = p2 = head;
   p2 = p2->next;
   long long cnt = 1;
-
   if (pos == 0) {
     return p2;
   }
-
   while (p2 != nullptr && cnt <= pos) {
     if (cnt == pos) {
       p1->next = p2->next;
@@ -84,14 +73,12 @@ Node *removeNode(Node *head, long long pos) {
     p1 = p1->next;
     p2 = p2->next;
   }
-
   return dummy->next;
 }
 
 Node *replaceAwithB(Node *head, long long a, long long b) {
   Node *dummy = createNode(0);
   dummy->next = head;
-
   while (head != nullptr) {
     if (head->data == a)
       head->data = b;
@@ -103,17 +90,14 @@ Node *replaceAwithB(Node *head, long long a, long long b) {
 Node *removeBigger(Node *head, long long a) {
   Node *dummy = createNode(0);
   dummy->next = head;
-
   Node *p1, *p2;
   p1 = p2 = head;
   p2 = p2->next;
-
   while (p1->data > a) {
     dummy->next = p1->next;
     p1 = p1->next;
     p2 = p2->next;
   }
-
   while (p2 != nullptr) {
     if (p2->data > a) {
       p1->next = p2->next;
@@ -123,7 +107,6 @@ Node *removeBigger(Node *head, long long a) {
       p2 = p2->next;
     }
   }
-
   return dummy->next;
 }
 
@@ -148,23 +131,16 @@ long long getDataAt(Node *head, long long pos) {
 
 signed main() {
   Node *lis = nullptr;
-
   long long n;
   cin >> n;
-
   for (long long i = 0; i < n; ++i) {
     long long temp;
     cin >> temp;
-
     insertBack(lis, temp);
   }
-
   long long a;
   cin >> a;
-
   lis = removeBigger(lis, getDataAt(lis, a));
-
   printLis(lis);
-
   return 0;
 }
