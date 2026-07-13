e9651886acff0668dd7375cfcb785c917053fb98
theBook273 <votansach273@gmail.com>
2026-07-13 12:51:52 +0700

    update

---
+++ sntDB/sntDb.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long main()
+{
+    long long n;
+
+    cin >> n;
+
+    while (n--)
+    {
+        long long a;
+
+        cin >> a;
+    }
+}
